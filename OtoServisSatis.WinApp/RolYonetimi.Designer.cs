b56752976d363dd142853479b3065cfe9784d8d1
Tuncay Çakıroğlu <100436327+TuncayCakr07@users.noreply.github.com>
2023-07-25 18:13:24 +0300

    Rol Ekleme İşlemi Yapıldı

---
+++ OtoServisSatis.WinApp/RolYonetimi.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace OtoServisSatis.WinApp
+{
+    partial class RolYonetimi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRoller = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnSil = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnGuncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.lblRolId = new System.Windows.Forms.Label();
+            this.TxtRolAdi = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblRol = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRoller)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvRoller
+            // 
+            this.dgvRoller.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvRoller.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRoller.Location = new System.Drawing.Point(3, 2);
+            this.dgvRoller.Name = "dgvRoller";
+            this.dgvRoller.RowHeadersWidth = 51;
+            this.dgvRoller.RowTemplate.Height = 24;
+            this.dgvRoller.Size = new System.Drawing.Size(350, 447);
+            this.dgvRoller.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnSil);
+            this.groupBox1.Controls.Add(this.BtnGuncelle);
+            this.groupBox1.Controls.Add(this.BtnEkle);
+            this.groupBox1.Controls.Add(this.lblRolId);
+            this.groupBox1.Controls.Add(this.TxtRolAdi);
+            this.groupBox1.Controls.Add(this.lblId);
+            this.groupBox1.Controls.Add(this.lblRol);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(359, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 182);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rol Bilgileri";
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Appearance.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSil.Appearance.Options.UseFont = true;
+            this.BtnSil.Location = new System.Drawing.Point(273, 119);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(105, 29);
+            this.BtnSil.TabIndex = 6;
+            this.BtnSil.Text = "SİL";
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Appearance.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGuncelle.Appearance.Options.UseFont = true;
+            this.BtnGuncelle.Location = new System.Drawing.Point(157, 119);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(110, 29);
+            this.BtnGuncelle.TabIndex = 5;
+            this.BtnGuncelle.Text = "GÜNCELLE";
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.Appearance.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnEkle.Appearance.Options.UseFont = true;
+            this.BtnEkle.Location = new System.Drawing.Point(49, 119);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(102, 29);
+            this.BtnEkle.TabIndex = 4;
+            this.BtnEkle.Text = "EKLE";
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // lblRolId
+            // 
+            this.lblRolId.AutoSize = true;
+            this.lblRolId.Location = new System.Drawing.Point(31, 43);
+            this.lblRolId.Name = "lblRolId";
+            this.lblRolId.Size = new System.Drawing.Size(60, 19);
+            this.lblRolId.TabIndex = 3;
+            this.lblRolId.Text = "Rol Id:";
+            // 
+            // TxtRolAdi
+            // 
+            this.TxtRolAdi.Location = new System.Drawing.Point(112, 71);
+            this.TxtRolAdi.Name = "TxtRolAdi";
+            this.TxtRolAdi.Size = new System.Drawing.Size(182, 28);
+            this.TxtRolAdi.TabIndex = 2;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(119, 43);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(19, 19);
+            this.lblId.TabIndex = 1;
+            this.lblId.Text = "0";
+            // 
+            // lblRol
+            // 
+            this.lblRol.AutoSize = true;
+            this.lblRol.Location = new System.Drawing.Point(19, 80);
+            this.lblRol.Name = "lblRol";
+            this.lblRol.Size = new System.Drawing.Size(72, 19);
+            this.lblRol.TabIndex = 0;
+            this.lblRol.Text = "Rol Adı:";
+            // 
+            // RolYonetimi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvRoller);
+            this.Name = "RolYonetimi";
+            this.Text = "RolYonetimi";
+            this.Load += new System.EventHandler(this.RolYonetimi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRoller)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRoller;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraEditors.SimpleButton BtnSil;
+        private DevExpress.XtraEditors.SimpleButton BtnGuncelle;
+        private DevExpress.XtraEditors.SimpleButton BtnEkle;
+        private System.Windows.Forms.Label lblRolId;
+        private System.Windows.Forms.TextBox TxtRolAdi;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblRol;
+    }
+}
---
+++ OtoServisSatis.WinApp/RolYonetimi.cs
@@ -0,0 +1,71 @@
+﻿using OtoServisSatis.BL;
+using OtoServisSatis.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtoServisSatis.WinApp
+{
+    public partial class RolYonetimi : Form
+    {
+        public RolYonetimi()
+        {
+            InitializeComponent();
+        }
+        RoleManager manager = new RoleManager();
+       
+        void Yukle()
+        {
+            dgvRoller.DataSource = manager.GetAll();
+        }
+
+        void Temizle()
+        {
+            TxtRolAdi.Text = string.Empty;
+            lblId.Text ="0";
+        }
+
+        private void RolYonetimi_Load(object sender, EventArgs e)
+        {
+            Yukle();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TxtRolAdi.Text.Trim().Length>0)
+                {
+                    var sonuc = manager.Add(new Rol
+                    {
+                        Adi = TxtRolAdi.Text,
+
+                    });
+                    if (sonuc > 0)
+                    {
+                        Temizle();
+                        Yukle();
+                        MessageBox.Show("Yeni Rol Eklendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Bilgileri Kontrol Ediniz!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hata Oluştu! Kayıt Eklenemedi!", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
